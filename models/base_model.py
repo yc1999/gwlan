46d70e937e76d3e11ba7f2e9fc5e6b52027be891
Carl Young <1043301600@qq.com>
2022-03-10 16:27:05 +0800

    add noam scheduler and modify setup

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,5 @@
 import pytorch_lightning as pl
+from torch.optim.lr_scheduler import LambdaLR
 
 class BaseModel(pl.LightningModule):
     @staticmethod
@@ -11,8 +12,22 @@ class BaseModel(pl.LightningModule):
         self.args = args
         self.save_hyperparameters(self.args)
     
+    def get_noam_schedule_with_warmup(self, optimizer, num_warmup_steps, num_training_steps, last_epoch=-1):
+        
+        def lr_lambda(current_step: int):
+            # print("current_step: " , current_step)
+            if current_step == 0:
+                return 0.0
+            ratio = (self.args.d_model ** (-0.5)) * (min( current_step ** (-0.5), current_step * num_warmup_steps**(-1.5) ))
+            return ratio
+        
+        return LambdaLR(optimizer, lr_lambda, last_epoch)
+        
     def setup(self, stage):
         if stage == "fit":
-            train_loader = self.trainer.datamodule.train_dataloader()
-            self.total_step = int( (self.trainer.max_epochs * len(train_loader) * self.trainer.limit_train_batches) / (self.trainer.gpus * self.trainer.accumulate_grad_batches) )
+            if self.trainer.max_steps == -1:
+                train_loader = self.trainer.datamodule.train_dataloader()
+                self.total_step = int( ( self.trainer.max_epochs * len(train_loader) )  / (self.trainer.gpus * self.trainer.accumulate_grad_batches) )
+            else:
+                self.total_step = self.trainer.max_steps
             print(f"Total optimization step is {self.total_step}")
